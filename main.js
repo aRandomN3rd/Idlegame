72bf073231ee3734565aaba7c2c0855b9b2ed1bd
aRandomN3rd <104280607+aRandomN3rd@users.noreply.github.com>
2022-06-04 15:28:40 -0400

    basic files, tried to add a cursor AND pick but it didn't work
    lmk if you can help with line the function buypick working and making it actually buy a pick. thanks

---
+++ main.js
@@ -0,0 +1,35 @@
+var gameData = {
+    Shib: 0,
+    Shib2: 0,
+    ShibPerClick: 1,
+    ShibPerClickCost: 10,
+    ShibPickCost: 25,
+    ShibPerPick: 5
+  }
+  
+  function mineShib() {
+    gameData.Shib += gameData.ShibPerClick
+    document.getElementById("ShibMined").innerHTML = gameData.Shib + " Shib Mined"
+  }
+  function buyPick() {
+    if (gameData.Shib2 >= gameData.ShibPerPickCost) {
+      gameData.Shib2 -= gameData.ShibPerPickCost
+      gameData.ShibPerPick += 5
+      gamedata.ShibPerPickCost *=2
+      document.getElementById("pickUpgrade").innerHTML = "Purchase Pickaxe (Currently Own " + gameData.ShibPerPick +  ") Cost: " + gameData.ShibPickCost + " Shib"
+    }
+  }
+  
+  function buyCursor() {
+    if (gameData.Shib >= gameData.ShibPerClickCost) {
+      gameData.Shib -= gameData.ShibPerClickCost
+      gameData.ShibPerClick += 1
+      gameData.ShibPerClickCost *= 2
+      document.getElementById("ShibMined").innerHTML = gameData.Shib + " Shib Mined"
+      document.getElementById("CursorUpgrade").innerHTML = "Purchase Cursor (Currently Own " + gameData.ShibPerClick + ") Cost: " + gameData.ShibPerClickCost + " Shib"
+    }
+  }
+  
+  var mainGameLoop = window.setInterval(function() {
+    mineShib() //reuse with dif var and function for incremental upgradess 1000 is in ms
+  }, 1000)
